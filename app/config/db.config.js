2a2087f6bff77daaa84dd8e865cb35727450b8de
lutfimaulana <lutfimaulana.fi@gmail.com>
2020-04-23 14:41:38 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "127.0.0.1",
-    USER: "postgres",
-    PASSWORD: "",
-    DB: "db_nodejs",
+    HOST: "ec2-23-20-129-146.compute-1.amazonaws.com",
+    USER: "vkekypdvwancqg",
+    PASSWORD: "fc8a043effdb7bd1291566a5a3556ec52a11ae726a3eb11613935071cef69b9d",
+    DB: "dca9kilikegb04",
     PORT: 5432
 }
